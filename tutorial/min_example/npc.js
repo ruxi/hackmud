fadde190c2f97a1003d7050bf3604d930e7bc758
ruxi <lewis.r.liu@gmail.com>
2016-10-02 01:40:00 -0500

    Create npc.js

---
+++ tutorial/min_example/npc.js
@@ -0,0 +1,16 @@
+// user.script -> message
+// user.script{} -> different message
+
+function(c, args){
+    var l = #s.scripts.lib();
+    // quasi-code
+    var msg1 = "hello world"
+    var msg2 = "continue dialog"
+    var actual_msg = ""
+    for(var i in args){
+      i = 0;
+      if (l.is_def(args[i]==True)} { i+=1 };
+      if (i>0){actual_msg=msg2}else{actual_msg=msg1
+      }
+    return actual_msg
+  }
